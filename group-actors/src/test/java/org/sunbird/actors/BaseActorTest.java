0f8cc23f653c0cc5d1e367714d449c9c9b446ccf
Aishwarya <42641684+aishwa8141@users.noreply.github.com>
2020-06-30 18:38:55 +0530

    SH-562: Add EmbeddedCassandra unit for mocking  (#6)
    * Merging play-seeder-without-router to groups and added test cases for actors
    
    * fixed BadTestControllerTest test case
    
    * removed BadController test
    
    * removed BadTestController test
    
    * group service version changes
    
    * updated play http secrete key
    
    * updated play http secrete key
    
    * merging playseeder without router changes to groups
    
    * added validation class and renamed module name
    
    * resolved merge conflict
    
    * SH-562: added EmbeddedCassandra unit for mocking cassandra
    
    * SH-562: added EmbeddedCassandra unit for mocking cassandra
    
    * ISSUE#SH-542: added requestId in testcase and updated create group query

--- group-actors/src/test/java/org/sunbird/actors/BaseActorTest.java
+++ group-actors/src/test/java/org/sunbird/actors/BaseActorTest.java
@@ -2,21 +2,34 @@ package org.sunbird.actors;
 
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.sunbird.util.JsonKey;
 
 public abstract class BaseActorTest {
 
   static ActorSystem system;
+  static Map<String, Object> headerMap = new HashMap<>();
 
   @BeforeClass
   public static void setup() {
     system = ActorSystem.create("system");
+    setReqId();
   }
 
   @AfterClass
   public static void teardown() {
     TestKit.shutdownActorSystem(system);
     system = null;
+  }
+
+  private static void setReqId() {
+    List<String> reqIds = new ArrayList<>();
+    reqIds.add("71ef3311-ac58-49a1-872b-7cf28159de83");
+    headerMap.put(JsonKey.REQUEST_MESSAGE_ID, reqIds);
   }
 }
