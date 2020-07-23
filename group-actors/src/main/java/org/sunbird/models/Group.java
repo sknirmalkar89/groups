ae8319f0984d1c259ee2965686be786ae1dd08b6
Aishwarya <42641684+aishwa8141@users.noreply.github.com>
2020-07-23 16:09:42 +0530

    SH-760:  fix null pointer exception while checking max activities limit in a group (#53)
    * SH-760: fix null pointer exception while checking maxActivityList
    
    * SH-760: review changes

--- group-actors/src/main/java/org/sunbird/models/Group.java
+++ group-actors/src/main/java/org/sunbird/models/Group.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,7 +20,7 @@ public class Group implements Serializable {
   private String description;
   private String status;
   private String membershipType;
-  private List<Map<String, Object>> activities;
+  private List<Map<String, Object>> activities = new ArrayList<>();
   private Timestamp createdOn;
   private String createdBy;
   private Timestamp updatedOn;
