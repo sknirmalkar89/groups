ec87be28d85b86a83195f7227949aed9d0866dc9
Aishwarya <42641684+aishwa8141@users.noreply.github.com>
2020-08-05 16:06:36 +0530

    SH-874: delete member cache if any updates happens to group (#62)
    * SH-874: delete cache userId after succesfully update group
    
    * SH-874: delete cache userId after succesfully update group

--- group-actors/src/main/java/org/sunbird/actors/UpdateGroupActor.java
+++ group-actors/src/main/java/org/sunbird/actors/UpdateGroupActor.java
@@ -58,40 +58,60 @@ public class UpdateGroupActor extends BaseActor {
     logger.info("Update group for the groupId {}", group.getId());
 
     String userId = group.getUpdatedBy();
-    if(StringUtils.isEmpty(userId)){
+    if (StringUtils.isEmpty(userId)) {
       throw new BaseException(
-              IResponseMessage.Key.UNAUTHORIZED_USER,
-              IResponseMessage.Message.UNAUTHORIZED_USER,
-              ResponseCode.UNAUTHORIZED.getCode());
+          IResponseMessage.Key.UNAUTHORIZED_USER,
+          IResponseMessage.Message.UNAUTHORIZED_USER,
+          ResponseCode.UNAUTHORIZED.getCode());
     }
 
     Map responseMap = null;
     // member validation and updates to group
+    MemberService memberService = new MemberServiceImpl();
+    List<MemberResponse> membersInDB = memberService.fetchMembersByGroupId(group.getId());
+
     if (MapUtils.isNotEmpty((Map) actorMessage.getRequest().get(JsonKey.MEMBERS))) {
-      responseMap = validateMembersAndSave(
+      responseMap =
+          validateMembersAndSave(
               group.getId(),
               (Map) actorMessage.getRequest().get(JsonKey.MEMBERS),
-              userId);
+              userId,
+              membersInDB);
     }
     // Activity validation
-    if (MapUtils.isNotEmpty((Map<String, Object>) actorMessage.getRequest().get(JsonKey.ACTIVITIES))) {
-      validateActivityList(group, (Map<String, Object>) actorMessage.getRequest().get(JsonKey.ACTIVITIES));
+    if (MapUtils.isNotEmpty(
+        (Map<String, Object>) actorMessage.getRequest().get(JsonKey.ACTIVITIES))) {
+      validateActivityList(
+          group, (Map<String, Object>) actorMessage.getRequest().get(JsonKey.ACTIVITIES));
     }
-
+    boolean deleteFromUserCache = false;
     // Group and activity updates
-    if( group!=null && (StringUtils.isNotEmpty(group.getDescription())
+    if (group != null
+        && (StringUtils.isNotEmpty(group.getDescription())
             || StringUtils.isNotEmpty(group.getName())
             || StringUtils.isNotEmpty(group.getMembershipType())
             || StringUtils.isNotEmpty(group.getStatus())
             || CollectionUtils.isNotEmpty(group.getActivities()))) {
       cacheUtil.delCache(group.getId());
+      // if name, description and status update happens in group , delete cache for all the members
+      // belongs to that group
+      deleteFromUserCache = true;
       GroupService groupService = new GroupServiceImpl();
       Response response = groupService.updateGroup(group);
     }
 
+    boolean isUseridRedisEnabled =
+        Boolean.parseBoolean(
+            PropertiesCache.getInstance().getConfigValue(JsonKey.ENABLE_USERID_REDIS_CACHE));
+    if (isUseridRedisEnabled) {
+      // Remove group list user cache from redis
+      deleteUserCache(
+          (Map) actorMessage.getRequest().get(JsonKey.MEMBERS), membersInDB, deleteFromUserCache);
+    }
+
     Response response = new Response(ResponseCode.OK.getCode());
     response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
-    if(MapUtils.isNotEmpty(responseMap) && ((List)responseMap.get(JsonKey.MEMBERS)).size()>0){
+    if (MapUtils.isNotEmpty(responseMap) && ((List) responseMap.get(JsonKey.MEMBERS)).size() > 0) {
       response.put(JsonKey.ERROR, responseMap);
     }
     sender().tell(response, self());
@@ -100,80 +120,83 @@ public class UpdateGroupActor extends BaseActor {
   }
 
   private void validateActivityList(Group group, Map<String, Object> activityOperationMap) {
-      List<Map<String, Object>> updateActivityList =
-              new GroupServiceImpl().handleActivityOperations(group.getId(), activityOperationMap);
-      GroupUtil.checkMaxActivityLimit(updateActivityList.size());
-      group.setActivities(updateActivityList);
+    List<Map<String, Object>> updateActivityList =
+        new GroupServiceImpl().handleActivityOperations(group.getId(), activityOperationMap);
+    GroupUtil.checkMaxActivityLimit(updateActivityList.size());
+    group.setActivities(updateActivityList);
   }
 
-  private Map validateMembersAndSave(String groupId, Map memberOperationMap, String requestedBy) {
+  private Map validateMembersAndSave(
+      String groupId,
+      Map memberOperationMap,
+      String requestedBy,
+      List<MemberResponse> membersInDB) {
     Map validationErrors = new HashMap<>();
     List errorList = new ArrayList();
-    validationErrors.put(JsonKey.MEMBERS,errorList);
+    validationErrors.put(JsonKey.MEMBERS, errorList);
 
     MemberService memberService = new MemberServiceImpl();
-    List<MemberResponse> membersInDB = memberService.fetchMembersByGroupId(groupId);
 
     GroupRequestHandler requestHandler = new GroupRequestHandler();
-    //Validate Member Addition
-    if(CollectionUtils.isNotEmpty((List<Map<String, Object>>) memberOperationMap.get(JsonKey.ADD))) {
+    // Validate Member Addition
+    if (CollectionUtils.isNotEmpty(
+        (List<Map<String, Object>>) memberOperationMap.get(JsonKey.ADD))) {
       requestHandler.validateAddMembers(memberOperationMap, membersInDB, validationErrors);
     }
-    //Validate Member Update
-    if(CollectionUtils.isNotEmpty((List<Map<String, Object>>) memberOperationMap.get(JsonKey.EDIT))) {
+    // Validate Member Update
+    if (CollectionUtils.isNotEmpty(
+        (List<Map<String, Object>>) memberOperationMap.get(JsonKey.EDIT))) {
       requestHandler.validateEditMembers(memberOperationMap, membersInDB, validationErrors);
     }
-    //Validate Member Remove
-    if(CollectionUtils.isNotEmpty((List<String>) memberOperationMap.get(JsonKey.REMOVE))){
-      requestHandler.validateRemoveMembers(memberOperationMap,  membersInDB, validationErrors);
+    // Validate Member Remove
+    if (CollectionUtils.isNotEmpty((List<String>) memberOperationMap.get(JsonKey.REMOVE))) {
+      requestHandler.validateRemoveMembers(memberOperationMap, membersInDB, validationErrors);
     }
     int totalMemberCount = GroupUtil.totalMemberCount(memberOperationMap, membersInDB);
     GroupUtil.checkMaxMemberLimit(totalMemberCount);
-
-    boolean isUseridRedisEnabled =
-        Boolean.parseBoolean(
-            PropertiesCache.getInstance().getConfigValue(JsonKey.ENABLE_USERID_REDIS_CACHE));
-    if (isUseridRedisEnabled) {
-      //Remove group list user cache from redis
-      deleteUserCache(memberOperationMap);
-    }
     cacheUtil.delCache(groupId + "_" + JsonKey.MEMBERS);
     memberService.handleMemberOperations(memberOperationMap, groupId, requestedBy);
 
     return validationErrors;
   }
 
-  private void deleteUserCache(Map memberOperationMap) {
-    List<Map<String, Object>> memberAddList =
-        (List<Map<String, Object>>) memberOperationMap.get(JsonKey.ADD);
-    if (CollectionUtils.isNotEmpty(memberAddList)) {
-      memberAddList.forEach(member -> cacheUtil.delCache((String) (member.get(JsonKey.USER_ID))));
+  private void deleteUserCache(
+      Map memberOperationMap, List<MemberResponse> dbMembers, boolean deleteFromUserCache) {
+    if (MapUtils.isNotEmpty(memberOperationMap)) {
+      List<Map<String, Object>> memberAddList =
+          (List<Map<String, Object>>) memberOperationMap.get(JsonKey.ADD);
+      if (CollectionUtils.isNotEmpty(memberAddList)) {
+        memberAddList.forEach(member -> cacheUtil.delCache((String) (member.get(JsonKey.USER_ID))));
+      }
+      List<Map<String, Object>> memberEditList =
+          (List<Map<String, Object>>) memberOperationMap.get(JsonKey.EDIT);
+      if (CollectionUtils.isNotEmpty(memberEditList)) {
+        memberEditList.forEach(
+            member -> cacheUtil.delCache((String) (member.get(JsonKey.USER_ID))));
+      }
+      List<String> memberRemoveList = (List<String>) memberOperationMap.get(JsonKey.REMOVE);
+      if (CollectionUtils.isNotEmpty(memberRemoveList)) {
+        memberRemoveList.forEach(member -> cacheUtil.delCache(member));
+      }
     }
-    List<Map<String, Object>> memberEditList =
-        (List<Map<String, Object>>) memberOperationMap.get(JsonKey.EDIT);
-    if (CollectionUtils.isNotEmpty(memberEditList)) {
-      memberEditList.forEach(member -> cacheUtil.delCache((String) (member.get(JsonKey.USER_ID))));
-    }
-    List<String> memberRemoveList = (List<String>) memberOperationMap.get(JsonKey.REMOVE);
-    if (CollectionUtils.isNotEmpty(memberRemoveList)) {
-      memberRemoveList.forEach(member -> cacheUtil.delCache(member));
+    if (CollectionUtils.isNotEmpty(dbMembers) && deleteFromUserCache) {
+      dbMembers.forEach(member -> cacheUtil.delCache(member.getUserId()));
     }
   }
 
-  private void logTelemetry(Request actorMessage, Group group){
+  private void logTelemetry(Request actorMessage, Group group) {
     Map<String, Object> targetObject = null;
     List<Map<String, Object>> correlatedObject = new ArrayList<>();
     if (null != group.getStatus() && JsonKey.INACTIVE.equals(group.getStatus())) {
       targetObject =
-              TelemetryUtil.generateTargetObject(
-                      group.getId(), TelemetryEnvKey.GROUP, JsonKey.DELETE, null);
+          TelemetryUtil.generateTargetObject(
+              group.getId(), TelemetryEnvKey.GROUP, JsonKey.DELETE, null);
     } else {
       targetObject =
-              TelemetryUtil.generateTargetObject(
-                      group.getId(), TelemetryEnvKey.GROUP, JsonKey.UPDATE, null);
+          TelemetryUtil.generateTargetObject(
+              group.getId(), TelemetryEnvKey.GROUP, JsonKey.UPDATE, null);
     }
     TelemetryUtil.telemetryProcessingCall(
-            actorMessage.getRequest(), targetObject, correlatedObject, actorMessage.getContext());
+        actorMessage.getRequest(), targetObject, correlatedObject, actorMessage.getContext());
   }
-
 }
