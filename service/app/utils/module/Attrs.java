e3d34b76b9a488460fea733f4cb50b247c7ebd1b
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2020-08-10 23:42:52 +0530

    SH-626 Adding log telemetry validation

--- service/app/utils/module/Attrs.java
+++ service/app/utils/module/Attrs.java
@@ -4,7 +4,8 @@ import org.sunbird.util.JsonKey;
 import play.libs.typedmap.TypedKey;
 
 public class Attrs {
-    public static final TypedKey<String> USERID = TypedKey.<String>create(JsonKey.USER_ID);
-    public static final TypedKey<String> CONTEXT = TypedKey.<String>create(JsonKey.CONTEXT);
-    public static final TypedKey<String> MANAGED_FOR = TypedKey.<String>create(JsonKey.MANAGED_FOR);
+  public static final TypedKey<String> USERID = TypedKey.<String>create(JsonKey.USER_ID);
+  public static final TypedKey<String> CONTEXT = TypedKey.<String>create(JsonKey.CONTEXT);
+  public static final TypedKey<String> MANAGED_FOR = TypedKey.<String>create(JsonKey.MANAGED_FOR);
+  public static final TypedKey<String> START_TIME = TypedKey.<String>create(JsonKey.START_TIME);
 }
