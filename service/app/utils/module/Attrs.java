d2ecf557e09a9845b8c2f0f7c624335f7ee17142
satish Nirmalkar <satishkumar.nirmalkar@tarento.com>
2020-08-11 12:34:52 +0530

    SB-20157 Log event validation issue fix

--- service/app/utils/module/Attrs.java
+++ service/app/utils/module/Attrs.java
@@ -4,14 +4,8 @@ import org.sunbird.util.JsonKey;
 import play.libs.typedmap.TypedKey;
 
 public class Attrs {
-<<<<<<< HEAD
   public static final TypedKey<String> USERID = TypedKey.<String>create(JsonKey.USER_ID);
   public static final TypedKey<String> CONTEXT = TypedKey.<String>create(JsonKey.CONTEXT);
   public static final TypedKey<String> MANAGED_FOR = TypedKey.<String>create(JsonKey.MANAGED_FOR);
   public static final TypedKey<String> START_TIME = TypedKey.<String>create(JsonKey.START_TIME);
-=======
-    public static final TypedKey<String> USERID = TypedKey.<String>create(JsonKey.USER_ID);
-    public static final TypedKey<String> CONTEXT = TypedKey.<String>create(JsonKey.CONTEXT);
-    public static final TypedKey<String> MANAGED_FOR = TypedKey.<String>create(JsonKey.MANAGED_FOR);
->>>>>>> 0d3d664a579cee0389b3b9e36f255ec99efd15cb
 }
